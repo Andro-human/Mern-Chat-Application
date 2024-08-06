9068367b02981ddeff1321e5f14117d0c38f4369
Animesh Sinha <animeshsinha1903@gmail.com>
2024-08-06 06:03:55 +0530

    Updated Cors policy

--- Backend/server.js
+++ Backend/server.js
@@ -36,7 +36,7 @@ const app = express();
 const server = createServer(app, {});
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://chat-app.animeshsinha.info"],
     credentials: true,
   },
 });
