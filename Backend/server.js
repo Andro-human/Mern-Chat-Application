3f088e80d2056aaacf887ec10c655a3af37bae86
Animesh Sinha <animeshsinha1903@gmail.com>
2024-08-06 12:53:19 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -37,6 +37,8 @@ const server = createServer(app, {});
 const io = new Server(server, {
   cors: {
     origin: ["http://localhost:5173", "https://chat-app.animeshsinha.info"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   },
 });
